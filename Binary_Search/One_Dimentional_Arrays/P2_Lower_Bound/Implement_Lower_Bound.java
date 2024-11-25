356ae85cfb9ca2ab6b77c12b9b4902fbb0f37e76
Surajit Rana <surajit1602@gmail.com>
2024-11-25 23:06:05 +0530

    feat[p2][binary-search][1d-array]: implement lower bound

--- Binary_Search/One_Dimentional_Arrays/P2_Lower_Bound/Implement_Lower_Bound.java
+++ Binary_Search/One_Dimentional_Arrays/P2_Lower_Bound/Implement_Lower_Bound.java
@@ -3,36 +3,54 @@ package Binary_Search.One_Dimentional_Arrays.P2_Lower_Bound;
 public class Implement_Lower_Bound {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 2, 3, 3, 5 };
-        int n = 6;
         int x = 2;
-        int lBound = lowerBound(arr, n, x);
-        System.out.println(lBound);
+
+        int lBoundBruteForce = lowerBoundBruteForce(arr, x);
+        System.out.println(lBoundBruteForce);
+
+        int lBoundOptimal = lowerBoundOptimal(arr, x);
+        System.out.println(lBoundOptimal);
     }
 
     /**
+     * Using Optimal Approach (Binary Search)
      * We need to find index where arr[i] >= x
      * 
      * TC: O(log(N))
      * SC: O(1)
      * 
      * @param arr
-     * @param n
-     * @param x
+     * @param target
      * @return
      */
-    public static int lowerBound(int[] arr, int n, int x) {
+    private static int lowerBoundOptimal(int[] arr, int target) {
+        int n = arr.length;
         int low = 0;
         int high = n - 1;
-        int answer = n;
-        while (low <= high) {
+        while (low <= high) { // TC: O(log(N))
             int mid = low + (high - low) / 2;
-            if (arr[mid] >= x) {
-                answer = mid; // best answer when high is decremented as below
+            if (arr[mid] >= target) {
                 high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
-        return answer;
+        return low;
+    }
+
+    /**
+     * Using Brute-Force Approach
+     * 
+     * TC: O(N)
+     * SC: O(1)
+     */
+    private static int lowerBoundBruteForce(int[] arr, int target) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            if (arr[i] >= target) {
+                return i;
+            }
+        }
+        return n;
     }
 }
