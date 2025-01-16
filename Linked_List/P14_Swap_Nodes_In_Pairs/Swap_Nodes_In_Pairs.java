7a02fc0978f9bff028b261611d1da458bf055576
Surajit Rana <surajit1602@gmail.com>
2025-01-16 20:44:46 +0530

    feat[linked-list][p14]: swap nodes in pairs

---
+++ Linked_List/P14_Swap_Nodes_In_Pairs/Swap_Nodes_In_Pairs.java
@@ -0,0 +1,34 @@
+package Linked_List.P14_Swap_Nodes_In_Pairs;
+
+import Linked_List.Utils.LinkedListUtils;
+import Linked_List.Utils.LinkedListUtils.ListNode;
+
+public class Swap_Nodes_In_Pairs {
+    public static void main(String[] args) {
+        Swap_Nodes_In_Pairs solution = new Swap_Nodes_In_Pairs();
+        int[] arr = { 1, 2, 3, 4 };
+
+        ListNode head = LinkedListUtils.createSinglyLinkedList(arr);
+        LinkedListUtils.printSinglyLinkedList(head);
+
+        ListNode resultList = solution.swapPairs(head);
+        LinkedListUtils.printSinglyLinkedList(resultList);
+    }
+
+    /**
+     * Using Recursion
+     * 
+     * TC: O(N)
+     * SC: O(N)
+     */
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode nextNode = head.next;
+        ListNode futureNode = nextNode.next;
+        head.next = swapPairs(futureNode);
+        nextNode.next = head;
+        return nextNode;
+    }
+}
