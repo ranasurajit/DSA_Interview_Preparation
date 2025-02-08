0865084e9d8b83386192776dc52034c53d0a95fa
Surajit Rana <surajit1602@gmail.com>
2025-02-09 02:02:24 +0530

    feat[dp][knapsack][p2]: subset sum problem

--- DP/KnapsackDP/P2_Subset_Sum_Problem/Subset_Sum_Problem.java
+++ DP/KnapsackDP/P2_Subset_Sum_Problem/Subset_Sum_Problem.java
@@ -17,11 +17,11 @@ public class Subset_Sum_Problem {
     }
 
     /**
-     * Using Tabulation
+     * Using DP Approach (Tabulation)
      *
-     * TC: O(N x K)
-     * SC: O(N x K)
-     * where K = target
+     * TC: O(N x T)
+     * SC: O(N x T)
+     * where T = target
      */
     private static boolean isSubsetSumTabulation(int arr[], int target) {
         int n = arr.length;
@@ -29,7 +29,8 @@ public class Subset_Sum_Problem {
          * creating dp array with states 'n' and 'target'
          * as they are variables denoting state changes
          */
-        boolean[][] dp = new boolean[n + 1][target + 1];
+        // initialization
+        Boolean[][] dp = new Boolean[n + 1][target + 1];
 
         // convert base case into initialization of 'dp' array
         /**
@@ -41,9 +42,9 @@ public class Subset_Sum_Problem {
          * so that can be done by {} empty array
          * so dp[0 to n][0] = true
          */
-        for (int j = 0; j < target + 1; j++) {
-            dp[0][j] = false;
-        }
+        // fill 0th row cells as False
+        Arrays.fill(dp[0], false);
+        // fill 0th column cells as True
         for (int i = 0; i < n + 1; i++) {
             dp[i][0] = true;
         }
@@ -51,15 +52,15 @@ public class Subset_Sum_Problem {
          * Now let's fill out the rest of the 'dp' array with values from recursive
          * code to iterative code
          * 
-         * Note: 'index' is transformed to 'i - 1' and 'target' to 'j'
+         * Note: transform (n, target) as (i , j)
          */
+        // iterative code
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < target + 1; j++) {
+                // transform (n, target) as (i , j)
                 if (arr[i - 1] <= j) {
-                    // pick || not pick
                     dp[i][j] = dp[i - 1][j - arr[i - 1]] || dp[i - 1][j];
                 } else {
-                    // we have no option to pick it
                     dp[i][j] = dp[i - 1][j];
                 }
             }
@@ -67,6 +68,13 @@ public class Subset_Sum_Problem {
         return dp[n][target];
     }
 
+    /**
+     * Using DP Approach (Memoization)
+     *
+     * TC: O(N x T)
+     * SC: O(N x T + N)
+     * where T = target
+     */
     private static boolean isSubsetSumMemoization(int arr[], int target) {
         int n = arr.length;
         /**
@@ -78,45 +86,40 @@ public class Subset_Sum_Problem {
         for (int[] dp1D : dp) {
             Arrays.fill(dp1D, -1);
         }
-        return solveMemoization(n - 1, arr, target, dp);
+        return solveMemoization(arr, target, n, dp);
     }
 
     /**
      * Using Memoization
      *
-     * TC: O(N x K)
-     * SC: O(N x K + N)
-     * where K = target
+     * TC: O(N x T)
+     * SC: O(N x T + N)
+     * where T = target
      */
-    private static Boolean solveMemoization(int index, int arr[], int target, int[][] dp) {
-        // base case
+    private static Boolean solveMemoization(int[] arr, int target, int n, int[][] dp) {
+        // Base case
         if (target == 0) {
             return true;
         }
-        if (index == 0) {
-            return arr[0] == target;
+        if (n == 0 && target != 0) {
+            return false;
         }
-        if (dp[index][target] != -1) {
-            return dp[index][target] == 1;
+        // memoization call
+        if (dp[n][target] != -1) {
+            return dp[n][target] == 1;
         }
-        /**
-         * if element at index of arr is <= target then only we have two
-         * choices
-         * 
-         * 1. pick
-         * 2. not pick
-         */
-        if (arr[index] <= target) {
-            // pick || not pick
-            boolean twoOptions = solveMemoization(index - 1, arr, target - arr[index], dp) ||
-                    solveMemoization(index - 1, arr, target, dp);
-            dp[index][target] = twoOptions ? 1 : 0;
-            return twoOptions;
+        // recursion call
+        if (arr[n - 1] <= target) {
+            // we have choice to pick or not pick
+            Boolean optionOne = (solveMemoization(arr, target - arr[n - 1], n - 1, dp)
+                    ||
+                    solveMemoization(arr, target, n - 1, dp));
+            dp[n][target] = optionOne ? 1 : 0;
+            return optionOne;
         } else {
-            // we have no option to pick it
-            boolean noOption = solveMemoization(index - 1, arr, target, dp);
-            dp[index][target] = noOption ? 1 : 0;
-            return noOption;
+            Boolean optionTwo = solveMemoization(arr, target, n - 1, dp);
+            dp[n][target] = optionTwo ? 1 : 0;
+            return optionTwo;
         }
     }
 
