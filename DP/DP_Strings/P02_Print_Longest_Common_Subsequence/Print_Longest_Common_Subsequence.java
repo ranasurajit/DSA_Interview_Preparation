80b06037dc45c0f5bafa298959401ab4c486485b
Surajit Rana <surajit1602@gmail.com>
2025-02-28 17:46:57 +0530

    feat[p1][dp][dp-strings]: longest common subsequence feat[p2][dp][dp-strings]: print longest common subsequence

---
+++ DP/DP_Strings/P02_Print_Longest_Common_Subsequence/Print_Longest_Common_Subsequence.java
@@ -0,0 +1,78 @@
+package DP.DP_Strings.P02_Print_Longest_Common_Subsequence;
+
+public class Print_Longest_Common_Subsequence {
+    public static void main(String[] args) {
+        Print_Longest_Common_Subsequence solution = new Print_Longest_Common_Subsequence();
+
+        int n = 5;
+        int m = 4;
+        String s1 = "abcab";
+        String s2 = "cbab";
+
+        String lcsString = solution.findLCS(n, m, s1, s2);
+        System.out.println(lcsString);
+    }
+
+    /**
+     * Using DP Bottom-Up (Tabulation) + Two-Pointers Approach
+     * 
+     * TC: O((N x M) + (N + M))
+     * SC: O((N x M) + (N + M))
+     * 
+     * @param n
+     * @param m
+     * @param s1
+     * @param s2
+     * @return
+     */
+    public String findLCS(int n, int m, String s1, String s2) {
+        // initialization
+        int[][] dp = new int[n + 1][m + 1]; // SC: O(N x M)
+        // if m = 0, then dp[m][0 to n] = 0
+        for (int i = 0; i < m + 1; i++) {
+            dp[0][i] = 0;
+        }
+        // if n = 0, then dp[0 to m][n] = 0
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = 0;
+        }
+        // iterative call
+        for (int i = 1; i < n + 1; i++) { // TC: O(N)
+            for (int j = 1; j < m + 1; j++) { // TC: O(M)
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        // Using Two-Pointers
+        StringBuilder sb = new StringBuilder(); // SC: O(N + M)
+        int i = n;
+        int j = m;
+        while (i > 0 && j > 0) { // TC: O(N + M)
+            if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                sb.append(s1.charAt(i - 1));
+                i--;
+                j--;
+            } else {
+                if (dp[i - 1][j] < dp[i][j - 1]) {
+                    sb.append(s1.charAt(i - 1));
+                    i--;
+                } else {
+                    sb.append(s2.charAt(j - 1));
+                    j--;
+                }
+            }
+        }
+        while (i > 0) {
+            sb.append(s1.charAt(i - 1));
+            i--;
+        }
+        while (j > 0) {
+            sb.append(s2.charAt(j - 1));
+            j--;
+        }
+        return sb.reverse().toString();
+    }
+}
---
+++ DP/DP_Strings/P02_Print_Longest_Common_Subsequence/README.md
@@ -0,0 +1,59 @@
+# Print Longest Common Subsequence
+
+## Problem Statement
+
+You are given two strings ‘s1’ and ‘s2’.
+
+Return the longest common subsequence of these strings.
+
+If there’s no such string, return an empty string. If there are multiple possible answers, return any such string.
+
+
+**Note:**
+Longest common subsequence of string ‘s1’ and ‘s2’ is the longest subsequence of ‘s1’ that is also a subsequence of ‘s2’. A ‘subsequence’ of ‘s1’ is a string that can be formed by deleting one or more (possibly zero) characters from ‘s1’.
+
+## Example
+
+### Example 1
+**Input:**
+```
+5 6
+ababa
+cbbcad
+```
+**Output:**
+```
+bba
+```
+**Explanation:**
+```
+“bba” is only possible longest subsequence present in both s1 = “ababa” and s2 = “cbbcad”. '1' is printed if the returned string is equal to "bba".
+```
+
+### Example 2
+**Input:**
+```
+3 3
+xyz
+abc
+```
+**Output:**
+```
+
+```
+**Explanation:**
+```
+There’s no subsequence of ‘s1’ that is also present in ‘s2’. Thus an empty string is returned and '1' is printed.
+```
+
+**Expected Time Complexity:**
+```
+Try to solve this in O(n*m). Where ‘n’ is the length of ‘s1’ and ‘m’ is the length of ‘s2’. 
+```
+
+**Constraints:**
+```
+1 <= n, m <= 10^3
+
+Time Limit: 1 sec
+```
