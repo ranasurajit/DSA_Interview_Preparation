7fd50dbf1a369f2520d7f5b121d1de526c14d985
Surajit Rana <surajit1602@gmail.com>
2025-03-18 20:11:22 +0530

    feat[p2][dp][dp-strings]: print longest common subsequence - corrected code

--- DP/DP_Strings/P02_Print_Longest_Common_Subsequence/Print_Longest_Common_Subsequence.java
+++ DP/DP_Strings/P02_Print_Longest_Common_Subsequence/Print_Longest_Common_Subsequence.java
@@ -1,5 +1,7 @@
 package DP.DP_Strings.P02_Print_Longest_Common_Subsequence;
 
+import java.util.Arrays;
+
 public class Print_Longest_Common_Subsequence {
     public static void main(String[] args) {
         Print_Longest_Common_Subsequence solution = new Print_Longest_Common_Subsequence();
@@ -9,7 +11,7 @@ public class Print_Longest_Common_Subsequence {
         String s1 = "abcab";
         String s2 = "cbab";
 
-        String lcsString1 = solution.findLCS(n, m, s1, s2);
+        String lcsString1 = solution.findLCSMemoization(n, m, s1, s2);
         System.out.println(lcsString1);
 
         n = 3;
@@ -17,15 +19,30 @@ public class Print_Longest_Common_Subsequence {
         String s3 = "xyz";
         String s4 = "abc";
 
-        String lcsString2 = solution.findLCS(n, m, s3, s4);
+        String lcsString2 = solution.findLCSMemoization(n, m, s3, s4);
         System.out.println(lcsString2);
+
+        n = 6;
+        m = 5;
+        String s5 = "abcdef";
+        String s6 = "abhef";
+
+        String lcsString3 = solution.findLCSTabulation(n, m, s5, s6);
+        System.out.println(lcsString3);
+
+        n = 3;
+        m = 3;
+        String s7 = "xyz";
+        String s8 = "pqr";
+        String lcsString4 = solution.findLCSTabulation(n, m, s7, s8);
+        System.out.println(lcsString4);
     }
 
     /**
-     * Using DP Bottom-Up (Tabulation) + Two-Pointers Approach
-     * 
-     * TC: O((N x M) + (N + M))
-     * SC: O((N x M) + (N + M))
+     * Approach II : Using Tabulation + Two Pointers Approach
+     *
+     * TC: O(N x M + (N + M) + Max(N, M)) ~ O(N x M + Max(N, M))
+     * SC: O(N x M)
      * 
      * @param n
      * @param m
@@ -33,18 +50,16 @@ public class Print_Longest_Common_Subsequence {
      * @param s2
      * @return
      */
-    public String findLCS(int n, int m, String s1, String s2) {
-        // initialization
+    public String findLCSTabulation(int n, int m, String s1, String s2) {
+        // Initialization
         int[][] dp = new int[n + 1][m + 1]; // SC: O(N x M)
-        // if m = 0, then dp[m][0 to n] = 0
-        for (int i = 0; i < m + 1; i++) {
-            dp[0][i] = 0;
-        }
-        // if n = 0, then dp[0 to m][n] = 0
-        for (int i = 0; i < n + 1; i++) {
+        for (int i = 0; i < n + 1; i++) { // TC: O(N)
             dp[i][0] = 0;
         }
-        // iterative call
+        for (int j = 0; j < m + 1; j++) { // TC: O(M)
+            dp[0][j] = 0;
+        }
+        // Iterative Calls
         for (int i = 1; i < n + 1; i++) { // TC: O(N)
             for (int j = 1; j < m + 1; j++) { // TC: O(M)
                 if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
@@ -54,33 +69,80 @@ public class Print_Longest_Common_Subsequence {
                 }
             }
         }
-        // Using Two-Pointers
-        StringBuilder sb = new StringBuilder(); // SC: O(N + M)
+        // Printing the LCS Using Two Pointers Approach
+        StringBuilder sb = new StringBuilder();
         int i = n;
         int j = m;
-        while (i > 0 && j > 0) { // TC: O(N + M)
+        while (i > 0 && j > 0) { // TC: O(Max(N, M))
+            if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                sb.insert(0, s1.charAt(i - 1));
+                i--;
+                j--;
+            } else {
+                if (dp[i - 1][j] > dp[i][j - 1]) {
+                    i--;
+                } else {
+                    j--;
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Approach I : Using Memoization + Two Pointers Approach
+     *
+     * TC: O(N x M + Max(N, M))
+     * SC: O(N x M + (N + M))
+     */
+    public String findLCSMemoization(int n, int m, String s1, String s2) {
+        int[][] memo = new int[n + 1][m + 1];
+        for (int[] memoItem : memo) {
+            Arrays.fill(memoItem, -1);
+        }
+        solveMemoization(n, m, s1, s2, memo);
+        // Printing the LCS Using Two Pointers Approach
+        StringBuilder sb = new StringBuilder();
+        int i = n;
+        int j = m;
+        while (i > 0 && j > 0) { // TC: O(Max(N, M))
             if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
                 sb.append(s1.charAt(i - 1));
                 i--;
                 j--;
             } else {
-                if (dp[i - 1][j] < dp[i][j - 1]) {
-                    sb.append(s1.charAt(i - 1));
+                if (memo[i - 1][j] > memo[i][j - 1]) {
                     i--;
                 } else {
-                    sb.append(s2.charAt(j - 1));
                     j--;
                 }
             }
         }
-        while (i > 0) {
-            sb.append(s1.charAt(i - 1));
-            i--;
-        }
-        while (j > 0) {
-            sb.append(s2.charAt(j - 1));
-            j--;
-        }
         return sb.reverse().toString();
+    }
+
+    /**
+     * Using Memoization
+     *
+     * TC: O(N x M)
+     * SC: O(N x M + (N + M))
+     */
+    private int solveMemoization(int n, int m, String s1, String s2, int[][] memo) {
+        // Base Case
+        if (m == 0 || n == 0) {
+            return 0;
+        }
+        // Memoization Check
+        if (memo[n][m] != -1) {
+            return memo[n][m];
+        }
+        // Recursion Calls
+        if (s1.charAt(n - 1) == s2.charAt(m - 1)) {
+            return memo[n][m] = 1 + solveMemoization(n - 1, m - 1, s1, s2, memo);
+        } else {
+            return memo[n][m] = Math.max(
+                    solveMemoization(n - 1, m, s1, s2, memo),
+                    solveMemoization(n, m - 1, s1, s2, memo));
+        }
     }
 }
