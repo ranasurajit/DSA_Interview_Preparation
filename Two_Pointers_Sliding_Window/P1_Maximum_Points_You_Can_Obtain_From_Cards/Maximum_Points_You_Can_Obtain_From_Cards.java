99f91332c94ccbec3ee25cb55d56cebbcf8ccc18
Surajit Rana <surajit1602@gmail.com>
2025-05-09 22:30:24 +0530

    feat[two-pointers][p1]: maximum points you can obtain from cards

---
+++ Two_Pointers_Sliding_Window/P1_Maximum_Points_You_Can_Obtain_From_Cards/Maximum_Points_You_Can_Obtain_From_Cards.java
@@ -0,0 +1,41 @@
+package Two_Pointers_Sliding_Window.P1_Maximum_Points_You_Can_Obtain_From_Cards;
+
+public class Maximum_Points_You_Can_Obtain_From_Cards {
+    public static void main(String[] args) {
+        Maximum_Points_You_Can_Obtain_From_Cards solution = new Maximum_Points_You_Can_Obtain_From_Cards();
+
+        int[] cardPoints = { 1, 2, 3, 4, 5, 6, 1 };
+        int k = 3;
+
+        int maximumScore = solution.maxScore(cardPoints, k);
+        System.out.println(maximumScore);
+    }
+
+    /**
+     * Approach : Using Two Pointers Approach
+     *
+     * TC: O(2 x K) ~ O(K)
+     * SC: O(1)
+     */
+    public int maxScore(int[] cardPoints, int k) {
+        int n = cardPoints.length;
+        int max = 0;
+        int leftSum = 0;
+        int rightSum = 0;
+        for (int i = 0; i < k; i++) { // TC: O(K)
+            leftSum += cardPoints[i];
+        }
+        max = leftSum;
+        int p = k - 1; // left pointer
+        int q = n - 1; // right pointer
+        while (p >= 0 && q >= 0 && k > 0) { // TC: O(K)
+            leftSum -= cardPoints[p];
+            rightSum += cardPoints[q];
+            p--;
+            q--;
+            k--;
+            max = Math.max(max, leftSum + rightSum);
+        }
+        return max;
+    }
+}
