cb665cdc94d1ad62ea88e3dc4c0c5d5a4d53ae99
Surajit Rana <surajit1602@gmail.com>
2025-05-18 21:22:29 +0530

    feat[binary-search][p2]: binary search in array with decreasing order elements

---
+++ Binary_Search/One_Dimensional/P2_Binary_Search_In_Array_With_Decreasing_Order_Elements/Binary_Search_In_Array_With_Decreasing_Order_Elements.java
@@ -0,0 +1,46 @@
+package Binary_Search.One_Dimensional.P2_Binary_Search_In_Array_With_Decreasing_Order_Elements;
+
+public class Binary_Search_In_Array_With_Decreasing_Order_Elements {
+    public static void main(String[] args) {
+        Binary_Search_In_Array_With_Decreasing_Order_Elements solution = new Binary_Search_In_Array_With_Decreasing_Order_Elements();
+
+        int[] arr1 = { 5, 4, 3, 2, 1 };
+        int k1 = 4;
+
+        int index1 = solution.binarysearch(arr1, k1);
+        System.out.println(index1);
+
+        int[] arr2 = { 2, 1, 1, 1, 1 };
+        int k2 = 1;
+        int index2 = solution.binarysearch(arr2, k2);
+        System.out.println(index2);
+    }
+
+    /**
+     * Approach : Using Binary Search
+     *
+     * TC: O(log(N))
+     * SC: O(1)
+     */
+    public int binarysearch(int[] arr, int k) {
+        int n = arr.length;
+        int low = 0;
+        int high = n - 1;
+        int index = -1;
+        while (low <= high) { // TC: O(log(N))
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == k) {
+                index = mid;
+                high = mid - 1;
+                // shrink the search space to left in case there are multiple values = k
+            } else if (arr[mid] < k) {
+                // search space is before mid index
+                high = mid - 1;
+            } else {
+                // search space is after mid index
+                low = mid + 1;
+            }
+        }
+        return index;
+    }
+}
